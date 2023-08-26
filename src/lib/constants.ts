b44396272bf1d79687de19f664041bf38bb14332
Ryan Roberts <ryanr@cityhive.net>
2023-08-26 11:25:49 +0300

    proper unocss setup

---
+++ src/lib/constants.ts
@@ -0,0 +1,1 @@
+export const CLOUDFRONT_DOMAIN = 'https://dk7xilc1p59yo.cloudfront.net/img/';
---
+++ src/ui/Header.svelte
@@ -0,0 +1,4 @@
+<header class="flex flex-col font-sans">
+	<h1>Colonoscopy Food Check</h1>
+	<h4>Check which food you can eat and when</h4>
+</header>
